612309be316595463b652aa000a921aaa4043851
Carlos <carlos.tur@escuelaestech.es>
2021-04-21 12:27:50 +0200

    Migraciones
    Migraciones tablas establecimientos, imágenes, pagos, anuncios, cartas y productos

---
+++ app/Http/Controllers/EstablecimientoController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Establecimiento;
+use Illuminate\Http\Request;
+
+class EstablecimientoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Establecimiento  $establecimiento
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Establecimiento $establecimiento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Establecimiento  $establecimiento
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Establecimiento $establecimiento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Establecimiento  $establecimiento
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Establecimiento $establecimiento)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_04_14_171834_create_establecimientos_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEstablecimientosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('establecimientos', function (Blueprint $table) {
+            $table->id();
+            
+            $table->string('nombre_establecimiento', 100);
+            $table->text('descripcion_establecimiento');
+            $table->string('dirección_establecimiento');
+            $table->double('latitud', 8, 2);
+            $table->double('longitud', 8, 2);
+            $table->string('tipo_establecimiento', 100);
+            $table->integer('nif');
+            $table->integer('maximo_numero_comensales');
+            $table->integer('aforo');
+            $table->string('ruta_foto_principal');
+            $table->integer('puntuacion_media_establecimiento');
+            $table->foreignId('propietario_id');
+            $table->boolean('es_premium');
+
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('establecimientos');
+    }
+}
