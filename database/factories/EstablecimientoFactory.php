402e4c47f98aa659b4e13dfe8c810ba551fb767d
Carlos <carlos.tur@escuelaestech.es>
2021-05-03 18:30:00 +0200

    Suprimidos anuncios y pagos
    Hemos decidido eliminar la posibilidad de poner anuncios y suscribirse, lo que conlleva que tampoco se ofrezca a los propietarios de los establecimientos visualizar sus métricas

--- database/factories/EstablecimientoFactory.php
+++ database/factories/EstablecimientoFactory.php
@@ -6,7 +6,25 @@ use App\Establecimiento;
 use Faker\Generator as Faker;
 
 $factory->define(Establecimiento::class, function (Faker $faker) {
+
     return [
+<<<<<<< Updated upstream
         //
+=======
+        'nombre_establecimiento' => $faker->company(),
+        'descripcion_establecimiento' => $faker->realText($maxNbChars = 200, $indexSize = 2),
+        'dirección_establecimiento' => $faker->address(),
+        'num_telefono' => 953600478,
+        'latitud' => $faker->latitude($min = -90, $max = 90),
+        'longitud' => $faker->longitude($min = -180, $max = 180),
+        'tipo_establecimiento' => $faker->randomElement([Establecimiento::TIPO_ESTABL_1,
+                                Establecimiento::TIPO_ESTABL_2, Establecimiento::TIPO_ESTABL_3]),
+        'nif' => 'J29746401',
+        'maximo_numero_comensales' => $faker->numberBetween($min = 50, $max = 100),
+        'aforo' => 100,
+        //'puntuacion_media_establecimiento' => 10,
+        'user_id' => User::all()->random()->id, //solo aquellos que sean de tipo propietarios
+        'es_premium' => $faker->randomElement([0, 1]),
+>>>>>>> Stashed changes
     ];
 });
--- routes/api.php
+++ routes/api.php
@@ -27,25 +27,19 @@ Route::apiResource('establecimientos.puntuacion', 'EstablecimientoPuntuacionCont
 Route::apiResource('establecimientos.productos', 'EstablecimientoProductoController');
 Route::apiResource('establecimientos.productos.puntuacion', 'EstablecimientoProductoPuntuacionController', ['only'=>['index']]);
 Route::apiResource('establecimientos.reservas', 'EstablecimientoReservaController');
-Route::apiResource('establecimientos.anuncios', 'EstablecimientoAnuncioController');
+
 
 Route::apiResource('usuarios', 'UserController');
 Route::apiResource('usuarios.tipos', 'UserTipoUsuarioController');
 Route::apiResource('usuarios.reservas', 'UserReservasController', ['only'=>['index', 'store']]);
 
-//Route::apiResource('productos', 'ProductoController');
-
 Route::apiResource('reservas', 'ReservaController');
-
-//Route::apiResource('imagen', 'ImagenController', ['except'=>['update']]);
 
 Route::apiResource('puntuaciones_establecimientos', 'PuntuacionEstablecimientoController');
 
 Route::apiResource('puntuaciones_productos', 'PuntuacionProductoController');
 
-Route::apiResource('anuncios', 'AnuncioController');
 
-Route::apiResource('pagos', 'PagoController');
 
 
 
